6e240737ad11db6fa94c1a5403445ae6065dd6ba
Piotr Karpala <pkarpala@infusion.com>
2015-08-01 22:53:55 +0200

    initital commit, working version

---
+++ Permut/Permut.Tests/UnitTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Permut.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        private IEnumerable<int> CreateReversedArray(int n)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                yield return i;
+            }
+        }
+
+        [TestMethod]
+        public void FindPermut_Should_WorkForBigN()
+        {
+            // Arrange
+            BigInteger i = BigInteger.Parse("30414093201713378043612608166064768844377641568960511999999999999");
+            int n = 50;
+            var expected = CreateReversedArray(n).ToArray();
+
+            // Act
+            var result = Program.FindIthPermutation(n, i);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Permut/Permut/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Permut
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == null || input.Length < 3)
+                    return;
+
+                var data = input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                var n = int.Parse(data[0]);
+                var i = BigInteger.Parse(data[1]);
+
+                var result = FindIthPermutation(n, i);
+                Console.WriteLine(ArrayToString(result));
+            }
+        }
+
+        static string ArrayToString(int[] array)
+        {
+            return string.Join(" ", array.Select(d => d.ToString()));
+        }
+
+        public static int[] FindIthPermutation(int n, BigInteger i)
+        {
+            BigInteger[] fact = new BigInteger[n];
+            int[] permutation = new int[n];
+
+            fact[0] = 1;
+            for (int k = 1; k < n; k++)
+            {
+                fact[k] = fact[k - 1] * k;
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                permutation[k] = (int)BigInteger.Divide(i, fact[n - 1 - k]);
+                i = i % fact[n - 1 - k];
+            }
+
+            for (int k = n - 1; k > 0; k--)
+            {
+                for (int j = k - 1; j >= 0; j--)
+                {
+                    if (permutation[j] <= permutation[k])
+                        permutation[k]++;
+                }
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                permutation[k]++;
+            }
+
+            return permutation;
+        }
+    }
+}
